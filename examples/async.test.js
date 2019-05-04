812b1e765216c125ad09e4b4f0ea4f6081a377e6
Andrew Ross <rossirsa@gmail.com>
2019-05-04 17:51:04 +0200

    Fixed code sandbox issue

--- examples/async.test.js
+++ examples/async.test.js
@@ -1,4 +1,6 @@
-jest.mock("react", () => require("test-react-hooks/ReactMock"));
+// doesn't work on code sandbox
+// uncomment to stop react from complaining about act not being called
+// jest.mock("react", () => require("test-react-hooks/ReactMock"));
 import { useEffect, useState } from "react";
 import { useTestProxy, cleanUp } from "test-react-hooks";
 
